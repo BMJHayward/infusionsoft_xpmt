68e2d7e3a6ae9e8398126c37a5a238e487b7c62a
BMJHayward <brendanhayward85@gmail.com>
2015-02-28 12:05:10 +1100

    infusion_test using unittest

--- infusion_test.py
+++ infusion_test.py
@@ -1,45 +1,51 @@
 """
 TODO: use
-import unittest, unittest.mock
 import cgi
 cgi.test()
 to test script with HTTP headers and HTML. See docs.python.org/3/library/cgi.html
 """
+import unittest, unittest.mock
 
 """ create connection to API, test data service and query function """
 import iteratequestionnaire as iq
 iqcxn = iq.InfusionQuery()
 
-def sampleQuery( ):
+class TestInfusionQuery(unittest.TestCase):
+    ''' this class may be just to test InfusionQuery() class, with new test class for each component class '''
+
+    def sampleQuery( ):
         """ makes 4 queries returning date created, leadsource and tags (groups) """
         table = 'Contact'
         returnFields = ['DateCreated', 'Leadsource']
         query = {'ContactType' : '%'}
         limit = 10
         page = 0
-        datesource = iqcxn.infusionsoft.DataService('query', 'Contact', \
+        datesource = iqcxn.infusionsoft.DataService('query', 'Contact',
                     10, 0, {'ContactType' : '%'}, ['DateCreated','Leadsource'])
 
-        print( iqcxn.infusionsoft.DataService( 'query', table, limit, \
-                    page, query, returnFields ))
-
-def fullStringQuery_a():
-        print(iqcxn.infusionsoft.DataService('query', 'Contact', 10, 0, \
-                     {'ContactType' : '%'}, ['DateCreated','Leadsource'])) # returns an array of dicts
-
-def fullStringQuery_b():
-        print(iqcxn.infusionsoft.DataService('query', 'Contact', 10, 0, \
-                    {'ContactType' : '%'}, ['Groups'])) # get tags with 'Groups' field from 'Contact' table
-
-def main():
-
-    tests = {'sample' : sampleQuery, 'query_a' : fullStringQuery_a, 'query_b' : fullStringQuery_b}
-
-    for test in tests.keys():
-        try:
-            tests[test]()
-        except Exception as detail:
-            print("query failed: " + test + ", ", detail)
+        print(iqcxn.infusionsoft.DataService( 'query', table, limit,
+                page, query, returnFields))
+    
+    def fullStringQuery_a():
+    
+        print(iqcxn.infusionsoft.DataService('query', 'Contact', 10, 0,
+                {'ContactType' : '%'}, ['DateCreated','Leadsource'])) # returns an array of dicts
+    
+    def fullStringQuery_b():
+    
+        print(iqcxn.infusionsoft.DataService('query', 'Contact', 10, 0,
+                {'ContactType' : '%'}, ['Groups'])) # get tags with 'Groups' field from 'Contact' table
+    
+    
+    def main():
+    
+        tests = {'sample' : sampleQuery, 'query_a' : fullStringQuery_a, 'query_b' : fullStringQuery_b}
+    
+        for test in tests.keys():
+            try:
+                tests[test]()
+            except Exception as detail:
+                print("query failed: " + test + ", ", detail)
 
 if __name__ == '__main__':
-    main()
+    unittest.main()
