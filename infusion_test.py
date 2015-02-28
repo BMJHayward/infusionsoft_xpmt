8d157f21ced5c3f2a5b5475f34d8f7462ae58b32
BMJHayward <brendanhayward85@gmail.com>
2015-02-28 12:28:13 +1100

    refactor to use unittest, remove old main() function

--- infusion_test.py
+++ infusion_test.py
@@ -1,10 +1,11 @@
 """
 TODO: use
+unittest.mock
 import cgi
 cgi.test()
 to test script with HTTP headers and HTML. See docs.python.org/3/library/cgi.html
 """
-import unittest, unittest.mock
+import unittest 
 
 """ create connection to API, test data service and query function """
 import iteratequestionnaire as iq
@@ -13,7 +14,7 @@ iqcxn = iq.InfusionQuery()
 class TestInfusionQuery(unittest.TestCase):
     ''' this class may be just to test InfusionQuery() class, with new test class for each component class '''
 
-    def sampleQuery( ):
+    def test_sampleQuery(self):
         """ makes 4 queries returning date created, leadsource and tags (groups) """
         table = 'Contact'
         returnFields = ['DateCreated', 'Leadsource']
@@ -26,26 +27,16 @@ class TestInfusionQuery(unittest.TestCase):
         print(iqcxn.infusionsoft.DataService( 'query', table, limit,
                 page, query, returnFields))
     
-    def fullStringQuery_a():
+    def test_fullStringQuery_a(self):
     
         print(iqcxn.infusionsoft.DataService('query', 'Contact', 10, 0,
                 {'ContactType' : '%'}, ['DateCreated','Leadsource'])) # returns an array of dicts
     
-    def fullStringQuery_b():
+    def test_fullStringQuery_b(self):
     
         print(iqcxn.infusionsoft.DataService('query', 'Contact', 10, 0,
                 {'ContactType' : '%'}, ['Groups'])) # get tags with 'Groups' field from 'Contact' table
     
-    
-    def main():
-    
-        tests = {'sample' : sampleQuery, 'query_a' : fullStringQuery_a, 'query_b' : fullStringQuery_b}
-    
-        for test in tests.keys():
-            try:
-                tests[test]()
-            except Exception as detail:
-                print("query failed: " + test + ", ", detail)
 
 if __name__ == '__main__':
     unittest.main()
