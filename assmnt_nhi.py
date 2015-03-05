7a94c94ad49c3da155a77670afc56646dedae0bc
Brendan Hayward <atomcogs@gmail.com>
2015-03-05 15:55:28 +1100

    query_tags() more sensible, remove white space from answers{}

--- assmnt_nhi.py
+++ assmnt_nhi.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python
+
 from dataserv import InfusionQuery
 import cgi
 import cgitb
@@ -22,30 +24,50 @@ current approach using dicts looks like it has potential
 '''
 
 answers = {  # get max of 'yes' answers from this list as result
-    'tinnitus_yes ': [1796, 1826, 1856, 1886],
-    'tinnitus_no ': [1800, 1830, 1860, 1890],
-    'tinnitus_sometimes ': [1798, 1828, 1858, 1888],
+    'tinnitus_yes': [1796, 1826, 1856, 1886],
+    'tinnitus_no': [1800, 1830, 1860, 1890],
+    'tinnitus_sometimes': [1798, 1828, 1858, 1888],
 
-    'hearing_yes ': [1808, 1838, 1868, 1898],
-    'hearing_no ': [1812, 1842, 1872, 1902],
-    'hearing_sometimes ': [1810, 1840, 1870, 1900],
+    'hearing_yes': [1808, 1838, 1868, 1898],
+    'hearing_no': [1812, 1842, 1872, 1902],
+    'hearing_sometimes': [1810, 1840, 1870, 1900],
 
-    'hyperacusis_yes ': [1802, 1832, 1862, 1892],
-    'hyperacusis_no ': [1806, 1836, 1866, 1896],
-    'hyperacusis_sometimes ': [1804, 1834, 1864, 1894],
+    'hyperacusis_yes': [1802, 1832, 1862, 1892],
+    'hyperacusis_no': [1806, 1836, 1866, 1896],
+    'hyperacusis_sometimes': [1804, 1834, 1864, 1894],
 
-    'dizziness_yes ': [1814, 1844, 1874, 1904],
-    'dizziness_no ': [1818, 1848, 1878, 1908],
-    'dizziness_sometimes ': [1816, 1846, 1876, 1906],
+    'dizziness_yes': [1814, 1844, 1874, 1904],
+    'dizziness_no': [1818, 1848, 1878, 1908],
+    'dizziness_sometimes': [1816, 1846, 1876, 1906],
 
-    'blockear_yes ': [1820, 1850, 1880, 1910],
-    'blockear_no ': [1824, 1854, 1884, 1914],
-    'blockear_sometimes ': [1822, 1852, 1882, 1912],
+    'blockear_yes': [1820, 1850, 1880, 1910],
+    'blockear_no': [1824, 1854, 1884, 1914],
+    'blockear_sometimes': [1822, 1852, 1882, 1912],
     }
 
+answer_list = list(iter(answers.values()))
 
 tag_result = {"tinnitus": 2278, "hyperacusis": 2280, "hearing": 2282,
               "dizziness": 2284, "blockear": 2286}
+
+
+def query_tags(contact_id):
+
+    returnFields = ["GroupId"]
+    query = {"ContactId": contact_id}
+    tags = infusionsoft.DataService("ContactGroupAssign", 100, 0,
+                                    query, returnFields)
+
+    return_tags = [item[tag] for item in tags for tag in item]
+
+    return return_tags
+
+
+def get_results(tag):
+
+    for item in answer_list:
+        for i in item:
+            print(tag == i)
 
 
 def iterateandAppend(list):  # used to compare result with client answers
@@ -54,29 +76,6 @@ def iterateandAppend(list):  # used to compare result with client answers
         for tag in value:
             list.append(tag)
     answers_list = [tag for values in answers.values() for tag in values]
-
-
-def query_tags(contact_id):
-
-    returnFields = ["GroupId"]
-    query = {"ContactID": contact_id}
-    tags = infusionsoft.DataService("ContactGroupAssign", 100, 0,
-                                    query, returnFields)
-    return tags
-
-    for tag in tags:
-        return_tags = tags[tag]
-
-    return return_tags
-
-
-def get_results(tag_id):
-
-    for results in questionnaire:
-        for answers in results:
-            for tags in answers:
-                if tags == tag_id:
-                    pass
 
 
 def score_update(target):
@@ -114,7 +113,7 @@ print ('highest score: ', highest_score)
 
 
 for results in comparison[results]:
-    if (results == highest_score && if  final_result == NULL):
+    if (results == highest_score and final_result is None):
         final_result[results] = results
 
 
@@ -134,7 +133,7 @@ data = (('assessment_type' , 'insert assessment type name'),
                 ('assessment_score_condition5', results[blockedear][scores])
               )
 
-infusionsoft.updateCon(contact_id, data)
-infusionsoft.grpAssign(contact_id, tag_result[final_result])
-print(contact_id)
-print(tag_result[final_result])
+  # infusionsoft.updateCon(contact_id, data)
+  # infusionsoft.grpAssign(contact_id, tag_result[final_result])
+  # print(contact_id)
+  # print(tag_result[final_result])
