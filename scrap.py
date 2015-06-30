0ffe5138435a11eb890a3cb0b6871316d66b90bf
BMJHayward <brendanhayward85@gmail.com>
2015-06-30 19:30:24 +1000

    remove code no longer used from scrap.py

--- scrap.py
+++ scrap.py
@@ -8,35 +8,6 @@ import sqlite3
 from datetime import date
 import time
 
-
-# this is straigh out of the docs for cmd module:
-def parse(arg):
-    'Convert a series of zero or more numbers to an argument tuple'
-    return tuple(map(int, arg.split()))
-
-def histogram():
-    '''
-    using bokeh to visualise:
-    >>> from bokeh.plotting import figure, output_file, show
-    >>> output_file('histogram.html')
-    >>> p = figure(title = 'insert title')
-    >>> x = datescount.keys()
-    >>> y = datescount.values()
-    >>> p.line(x,y)
-    >>> show(p)
-    '''
-
-    dates = eval(open('dates.txt', 'r+').read())
-    from collections import Counter
-    datescount = Counter(dates)
-
-    return datescount
-
-def sourcelist(cxn):
-    testlist = [cxn.dates(), cxn.tags(), cxn.leadsources()]
-
-    return testlist
-
 def filelinecount(filename):
     if type(filename) != str:
         filename = str(filename)
@@ -56,19 +27,6 @@ def linecount():
             except: pass
 
     return sum(numlines)
-
-def quickdbtest():
-    salesdata = dataserv.LocalDB().get_csv('sales.csv')
-    try:
-        dataserv.LocalDB().sendto_sqlite(salesdata, 'sales', db='salesdatatest.sqlite')
-    except sqlite3.OperationalError as e:
-        print('Please delete table and try again:\n', e, e.__class__, sys.exc_info()[2])
-
-def datestringtotuple(datestring):
-    datstr = datestring.split('/')
-    datstr = ''.join(datstr)
-    datstr = time.strptime(datstr, '%d%m%Y')
-    return datstr
 
 def get_db_column(dbname, dbtbl, dbcol):
     conn=sqlite3.connect(dbname)
