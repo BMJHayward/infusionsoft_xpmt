14cfdb6048cc6dffe4d1f921f32ac212428dfce6
Brendan Hayward <atomcogs@gmail.com>
2015-07-02 12:34:41 +1000

    dates args passed matplot_bar() and pandas_histogram()

--- matplotchart.py
+++ matplotchart.py
@@ -11,7 +11,7 @@ datescount = Counter(dates)
 datescount[20080627] = 500
 # this value skews the graph too much, original value is approx 23K
 
-def matplot_bar():
+def matplot_bar(datescount):
     labels, values = zip(*datescount.items())
 
     indexes = np.arange(len(labels))
@@ -21,7 +21,7 @@ def matplot_bar():
     plt.xticks(indexes + width * 0.5, labels)
     plt.show()
 
-def pandas_histogram():
+def pandas_histogram(dates):
     datesdf = pd.DataFrame(dates)
     plt.figure()
     datesdf.plot(kind='hist', alpha=0.5)
