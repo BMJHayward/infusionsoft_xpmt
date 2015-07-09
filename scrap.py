27e9addd716895d38c9d1fb85e56d20d331de5b8
Brendan Hayward <atomcogs@gmail.com>
2015-07-09 16:35:00 +1000

    column2datetype converts to date type. currently returns year only

--- scrap.py
+++ scrap.py
@@ -49,8 +49,9 @@ def column2datetype(db, table, column):
     doi = [list(i) for i in doi]
     for date in doi:
         date[1] = dateconvert.convert_datestring(date[1])
-        c.execute('UPDATE ? SET ? = ? WHERE rowid=?;',(table, column, date[1], date[0]))
-
+        c.execute('UPDATE {0} SET {1} = {2} WHERE rowid={3};'.format(table, column, date[1], date[0]))
+    conn.commit()
+    conn.close()
 
 if __name__ == "__main__":
     x = linecount()
