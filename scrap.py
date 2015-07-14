e03e8b043678eadf709b668e577e6a999db81ae4
BMJHayward <brendanhayward85@gmail.com>
2015-07-14 14:06:20 +1000

    use format() to print dates

--- scrap.py
+++ scrap.py
@@ -40,15 +40,15 @@ def plottest():
 def datefromcsv():
     reader = csv.DictReader(fil, delimiter=';')         #read the csv file
     for row in reader:
-        date = datetime.strptime(row['Dispatch date'], '%Y-%m-%d %H:%M:%S')     #datetime value in the right date format
+        date = datetime.strptime(row['Order Date'], '%Y-%m-%d %H:%M:%S')     #datetime value in the right date format
         values[date.strftime('%Y-%m-%d')] += 1          #increment the date with a step of 1
 
     for date, value in sorted(values.items()):
-        result = (value/ 3927.2) * 100          #Sla calcul with the theoritic number of line
-        print 'Le fichier date du %s' % date        #SLA display
-        print 'Le SLA est de : %d%%' % result
+        result = (value / 3927.2) * 100          #Sla calcul with the theoritic number of line
+        print 'Date: {}'.format(date)        #SLA display
+        print 'Result: {}'.format(result)
 
-    
+
 if __name__ == "__main__":
     x = linecount()
     print(x)
