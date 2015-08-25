043527bcaefc5d49fc532825e4d327bcd659dc26
Brendan Hayward <atomcogs@gmail.com>
2015-08-25 15:19:29 +1000

    fix datefromcsv

--- src/scrap.py
+++ src/scrap.py
@@ -38,16 +38,16 @@ def plottest():
     dates, values = qdata['Order Date'], qdata['Order Total']
     mpc.plotdates(dates, values)
 
-def datefromcsv():
-    reader = csv.DictReader(fil, delimiter=';')         #read the csv file
+def datefromcsv(file):
+    reader = csv.DictReader(file, delimiter=';')         #read the csv file
     for row in reader:
         date = datetime.strptime(row['Order Date'], '%Y-%m-%d %H:%M:%S')     #datetime value in the right date format
         values[date.strftime('%Y-%m-%d')] += 1          #increment the date with a step of 1
 
     for date, value in sorted(values.items()):
         result = (value / 3927.2) * 100          #Sla calcul with the theoritic number of line
-        print 'Date: {}'.format(date)        #SLA display
-        print 'Result: {}'.format(result)
+        print('Date: {}'.format(date))        #SLA display
+        print('Result: {}'.format(result))
 
 
 if __name__ == "__main__":
