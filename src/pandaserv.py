355f8df075bf1fcdd70dfe4855fab701572ba2c8
Brendan Hayward <atomcogs@gmail.com>
2015-08-27 15:55:52 +1000

    include try...except for encodings in read_csv()

--- src/pandaserv.py
+++ src/pandaserv.py
@@ -1,8 +1,17 @@
 import pandas as pd
 import os
+encodings = {
+            'enc_iso' : 'ISO-8859-1',
+            'enc_utf' : 'utf-8',
+            'enc_win' : 'cp1252',
+            }
 
 from dataserv import RAW_DATA_FILE
 
 raw_data = os.listdir( RAW_DATA_FILE )
-
-data_sheets = [pd.read_csv(datafile) for datafile in raw_data]
+data_sheets = []
+for encs in encodings:
+    try:
+        data_sheets = [pd.read_csv(datafile) for datafile in raw_data]
+    except UnicodeDecodeError:
+        data_sheets = [pd.read_csv(datafile, encoding=enc) for datafile in raw_data]
