1208df626e0f400d4297a132d4403ff9e8b5874e
Brendan Hayward <atomcogs@gmail.com>
2015-08-27 16:20:23 +1000

    move for loop under except clause for encodings

--- src/pandaserv.py
+++ src/pandaserv.py
@@ -1,17 +1,18 @@
 import pandas as pd
 import os
+from dataserv import RAW_DATA_FILE
+
 encodings = {
             'enc_iso' : 'ISO-8859-1',
             'enc_utf' : 'utf-8',
             'enc_win' : 'cp1252',
             }
 
-from dataserv import RAW_DATA_FILE
-
 raw_data = os.listdir( RAW_DATA_FILE )
+
 data_sheets = []
+try:
+data_sheets = [pd.read_csv(datafile) for datafile in raw_data]
+except UnicodeDecodeError:
 for encs in encodings:
-    try:
-        data_sheets = [pd.read_csv(datafile) for datafile in raw_data]
-    except UnicodeDecodeError:
-        data_sheets = [pd.read_csv(datafile, encoding=enc) for datafile in raw_data]
+    data_sheets = [pd.read_csv(datafile, encoding=enc) for datafile in raw_data]
