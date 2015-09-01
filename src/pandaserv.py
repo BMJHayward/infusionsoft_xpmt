bfa6bd2d06ebd269ad720cdaaa8373fce008f163
Brendan Hayward <atomcogs@gmail.com>
2015-09-01 16:22:22 +1000

    shell session to update pandas dataframe column

--- src/pandaserv.py
+++ src/pandaserv.py
@@ -3,9 +3,9 @@ import os
 from dataserv import RAW_DATA_FILE
 
 encodings = {
-            'enc_iso' : 'ISO-8859-1',
-            'enc_utf' : 'utf-8',
-            'enc_win' : 'cp1252',
+            'iso' : 'ISO-8859-1',
+            'utf' : 'utf-8',
+            'win' : 'cp1252',
             }
 
 raw_data = os.listdir( RAW_DATA_FILE )
@@ -16,3 +16,10 @@ try:
 except UnicodeDecodeError:
     for encs in encodings:
         data_sheets = [pd.read_csv(datafile, encoding=enc) for datafile in raw_data]
+
+how_to_stripcurrency = '''\
+                lsroi = pd.read_csv('lsroi.csv')
+                lsroi.loc[:, 'Expenses'] = float(lsroi['Expenses'].str.strip('AUD'))
+                lsroi.loc[:, 'Expenses'] = float(lsroi['Expenses'].str.replace(',', ''))
+                lsroi.loc[:, 'Expenses'] = lsroi['Expenses'].astype(float)\
+                '''
