7b96ef81c68f1103e1b339168161a7a7303d2f06
Brendan Hayward <atomcogs@gmail.com>
2015-09-18 15:22:41 +1000

    refactor make_sheets() to try...except each encoding for csv files

--- src/pandaserv.py
+++ src/pandaserv.py
@@ -32,21 +32,27 @@ def dframe_currencystrip(dframe, col, currency=currency):
     return dframe
 
 def make_sheets():
-    ''' Goes through files in RAW_DATA_DIR and returns a dict of
+    ''' Goes through files in RAW_DATA_DIR and returns
+        a dict of:
         {filename: pandas.DataFrame(filename)}
     '''
     data_sheets = {}
-    try:
-        data_sheets = {datafile.split()[0]: pd.read_csv(datafile) for datafile in raw_data}
-    except UnicodeDecodeError:
-        for enc in encodings:
-            data_sheets = {datafile.split()[0]: pd.read_csv(datafile, encoding=enc) for datafile in raw_data}
+    for enc in encodings:
+        try:
+            data_sheets = {datafile.split()[0]: pd.read_csv(datafile) for datafile in raw_data}
+            break
+        except UnicodeDecodeError:
+            data_sheets = {datafile.split()[0]: pd.read_csv(datafile, encoding=encodings[enc]) for datafile in raw_data}
+            break
+        except UnicodeDecodeError:
+            continue  # looks silly but allows attempt at next encoding in dict
     return data_sheets
 
 def clean_sheets(currency = currency):
     ''' Uses make_sheets(), and then processes to convert money from string to float,
         and datetime strings into datetime objects.
-        Returns a dict of {filename: pandas.DataFrame(filename)}
+        Returns a dict of:
+        {filename: pandas.DataFrame(filename)}
     '''
     data_sheets = make_sheets()
     for dframe in data_sheets:
