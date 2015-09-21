0a9de77c3770d07b2d91c5a4eeaf47cc3c5e4f89
Brendan Hayward <atomcogs@gmail.com>
2015-09-21 14:37:47 +1000

    update test_dframe_dateconv

--- test/pandaservtest.py
+++ test/pandaservtest.py
@@ -1,15 +1,32 @@
 import unittest, sys, os
 from datetime import datetime
+import pandas as pd
+import src.pandaserv as pandaserv
+import numpy as np
+
 
 class Testpandaserv(unittest.TestCase):
+    def setUp(self):
+        self.dates = pd.date_range('20130101', periods=6)
+        self.df = pd.DataFrame(
+                    np.random.randn(6,4), index=self.dates, columns=list('ABCD'))
+        self.df2 = pd.DataFrame({ 'A' : 1.,
+                                  'B' : pd.Timestamp('20130102'),
+                                  'C' : pd.Series(1,index=list(range(4)),dtype='float32'),
+                                  'D' : np.array([3] * 4,dtype='int32'),
+                                  'E' : pd.Categorical(["test","train","test","train"]),
+                                  'F' : 'foo' })
     def test_dframe_dateconv(self):
         print('Unfinished test, PASS.')
+        pandaserv.dframe_dateconv(self.df, 'D')
+        self.assertIsInstance(self.df['D'], datetime)
     def test_dframe_currencystrip(self):
         print('Unfinished test, PASS.')
     def test_make_sheets(self):
         print('Unfinished test, PASS.')
     def test_clean_sheets(self):
         print('Unfinished test, PASS.')
+
 
 if __name__ == '__main__':
     unittest.main()
