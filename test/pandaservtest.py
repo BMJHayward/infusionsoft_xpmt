c785a5135a65e76eb4dbc7eabdb7335779a0c18c
Brendan Hayward <atomcogs@gmail.com>
2015-09-23 15:47:02 +1000

    test clean_sheets() returns a pandas.DataFrame object

--- test/pandaservtest.py
+++ test/pandaservtest.py
@@ -19,10 +19,17 @@ class Testpandaserv(unittest.TestCase):
                                   'D' : np.array([3] * 4,dtype='int32'),
                                   'E' : pd.Categorical(["test","train","test","train"]),
                                   'F' : '20140101' })
-        self.moneydf = pd.DataFrame({'transactions': ['AUD1,234.01',
-                                    '-AUD1,234.01',
-                                    'AUD234.01',
-                                    'AUD1,234',]})
+        self.moneydf = pd.DataFrame({'transactions':
+                                        ['AUD1,234.01',
+                                        '-AUD1,234.01',
+                                        'AUD234.01',
+                                        'AUD1,234',],
+                                    'Order Total':
+                                        ['AUD1,234.01',
+                                        '-AUD1,234.01',
+                                        'AUD234.01',
+                                        'AUD1,234',]})
+        self.datesdf = pd.DataFrame(self.dates)
         try:
             os.mkdir('rawdata')
         except FileExistsError:
@@ -30,6 +37,7 @@ class Testpandaserv(unittest.TestCase):
         self.df.to_csv(path_or_buf='rawdata/df.csv')
         self.df2.to_csv(path_or_buf='rawdata/df2.csv')
         self.moneydf.to_csv(path_or_buf='rawdata/moneydf.csv')
+        self.datesdf.to_csv(path_or_buf='rawdata/datesdf.csv')
 
     def test_dframe_dateconv(self):
         pandaserv.dframe_dateconv(self.df2, 'F')
@@ -37,22 +45,24 @@ class Testpandaserv(unittest.TestCase):
             self.assertIsInstance(self.df2['F'][singledate], datetime)
 
     def test_dframe_currencystrip(self):
-        print('Unfinished test, PASS.')
         pandaserv.dframe_currencystrip(self.moneydf, 'transactions')
         for trxn in self.moneydf['transactions']:
-            self.assertNotIsInstance(trxn, str)
-            self.assertIsInstance(trxn, np.float64)
+            self.assertIsNot(type(trxn), str)
+            self.assertIs(type(trxn), float)
 
     def test_make_sheets(self):
-        print('Unfinished test, PASS.')
-        self.test_sheets = pandaserv.make_sheets()
-        self.assertIsInstance(self.test_sheets, dict)
-        for sheet in self.test_sheets:
+        self.made_sheets = pandaserv.make_sheets()
+        self.assertIsInstance(self.made_sheets, dict)
+        for sheet in self.made_sheets:
             self.assertIsInstance(sheet, str)
-            self.assertIsInstance(self.test_sheets[sheet], pd.DataFrame)
+            self.assertIsInstance(self.made_sheets[sheet], pd.DataFrame)
 
     def test_clean_sheets(self):
         print('Unfinished test, PASS.')
+        self.cleaned_sheets = pandaserv.clean_sheets()
+        self.assertIs(type(self.cleaned_sheets), dict)
+        for filename, dframe in self.cleaned_sheets:
+            self.assertIs(type(dframe), pd.DataFrame)
 
 
 if __name__ == '__main__':
